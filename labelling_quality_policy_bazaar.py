de8ae612237fb2b470e1d3bab9bb25acab39ca6e
manabroyobs <manabroyobs@observancegroup.com>
2023-11-27 11:17:56 +0000

    Update code

--- labelling_quality_policy_bazaar.py
+++ labelling_quality_policy_bazaar.py
@@ -6,8 +6,8 @@ run_name = f"Policy-Bazaar-labeling-quality-{datetime.datetime.now().strftime('%
 # print("******************")
 test_session = TestSession(project_name="testingProject", run_name= run_name, access_key="y2EkSDP1yryEpHAzuJO8", secret_key="DR7ZZt22LVe0iIJ1psG2beegUmaQ53vTUhpX5Fg4", host="http://3.111.106.226:8080")
 
-rules = LQRules()
-rules.add(metric="mistake_score", label=["ALL"], metric_threshold=0.065)
+rules = LQRules_PB()
+rules.add(metric="mistake_score", label=["ALL"])
 
 
 edge_case_detection = labelling_quality_test_PB(test_session=test_session,
