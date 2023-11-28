915bc031d928768a56fe370753016c62db824d71
manabroyobs <manabroyobs@observancegroup.com>
2023-11-28 07:37:40 +0000

    Update code

--- data_loaders/nanonet_data_loaders/nano_dataset_upload_missing_values.py
+++ data_loaders/nanonet_data_loaders/nano_dataset_upload_missing_values.py
@@ -75,7 +75,7 @@ schema.add("Recognition", ObjectRecognitionSchemaElement(model="nanonet_model", 
 run_name = f"Nano_Net_Dataset-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
 
 # create test_session object of TestSession instance
-test_session = TestSession(project_name="testingProject", run_name= run_name, profile="nanonet_dev")
+test_session = TestSession(project_name="testingProject", run_name= run_name, profile="raga-dev-new")
 
 cred = DatasetCreds(region="us-east-2")
 
--- tests/policy_bazaar_tests/policy_bazaar_LQ_test.py
+++ tests/policy_bazaar_tests/policy_bazaar_LQ_test.py
@@ -1,14 +1,17 @@
 from raga import *
 import datetime
-from raga.test_config import labelling_quality_test
-run_name = f"poc-test-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
+
+run_name = f"policy_bazaar-test-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
 
 # create test_session object of TestSession instance
-test_session = TestSession(project_name="testingProject", run_name= run_name, profile="dev")
+test_session = TestSession(project_name="testingProject", run_name= run_name, profile="raga-dev-new")
 
-dataset_name = "policy_bazaar_train_dataset"
+rules = LC()
+rules.add(metric="mistake_score", label=["ALL"])
 
-edge_case_detection = labelling_quality_test(test_session=test_session,
+dataset_name = "Enter_your_dataset_name"
+
+edge_case_detection = labelling_quality_test_PB(test_session=test_session,
                                              dataset_name = dataset_name,
                                              test_name = "pb_labelling_quality_2",
                                              trainModelColumnName = "target",
@@ -18,6 +21,6 @@ edge_case_detection = labelling_quality_test(test_session=test_session,
                                              embeddingTrainColName = "embedding",
                                              embeddingFieldColName = "embedding",
                                              rules = rules)
+test_session.add(edge_case_detection)
 
-
-
+test_session.run()
