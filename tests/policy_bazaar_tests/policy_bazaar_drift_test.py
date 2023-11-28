26876143fab3de1ff223a89e33ae4f11c5b27a38
manabroyobs <manabroyobs@observancegroup.com>
2023-11-28 07:44:01 +0000

    Update code

---
+++ tests/policy_bazaar_tests/policy_bazaar_drift_test.py
@@ -0,0 +1,26 @@
+from raga import *
+import datetime
+
+run_name = f"policy_bazaar-test-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
+
+# create test_session object of TestSession instance
+test_session = TestSession(project_name="testingProject", run_name= run_name, profile="raga-dev-new")
+
+rules = LC()
+rules.add(metric="mistake_score", label=["ALL"])
+
+dataset_name = "Enter_your_dataset_name"
+
+edge_case_detection = labelling_quality_test_PB(test_session=test_session,
+                                             dataset_name = dataset_name,
+                                             test_name = "pb_labelling_quality_2",
+                                             trainModelColumnName = "target",
+                                             fieldModelColumnName = "target",
+                                             type = "labelling_consistency",
+                                             output_type="embedding_data",
+                                             embeddingTrainColName = "embedding",
+                                             embeddingFieldColName = "embedding",
+                                             rules = rules)
+test_session.add(edge_case_detection)
+
+test_session.run()
