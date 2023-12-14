2c5d2475b3b9bd54935cacc22d25169fc60ad4a1
manabroyobs <manabroyobs@observancegroup.com>
2023-12-14 08:20:03 +0000

    Update code

--- tests/near_duplicate_tests/near_duplicate.py
+++ tests/near_duplicate_tests/near_duplicate.py
@@ -5,19 +5,17 @@ run_name = f"Near-Duplicate-{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
 # print(run_name)
 # print("**********")
 # create test_session object of TestSession instance
-# test_session = TestSession(project_name="testingProject", run_name= run_name, profile="dev")
 test_session = TestSession(project_name="testingProject", run_name= run_name, profile="dev1")
 
 rules = LQRules()
 rules.add(metric="similarity_score", metric_threshold=0.99)
-
 
 dataset_name = "Enter-your-dataset-name"
 
 edge_case_detection = nearest_duplicate(test_session=test_session,
                                           dataset_name = dataset_name,
                                           test_name = "near_duplicate_detection_1",
-                                          type = "nearest_neighbour",
+                                          type = "near_duplicates",
                                           output_type="near_duplicates",
                                           embed_col_name="embedding",
                                           rules=rules)
